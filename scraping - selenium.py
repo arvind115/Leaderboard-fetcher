fd927f5f3b818f396a111641ae406276f2fe344d
Arvind Kumar Singh <samratarvind114@gmail.com>
2019-08-26 09:19:32 +0530

    added a few performance improverments

--- scraping - selenium.py
+++ scraping - selenium.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 import pandas as pd
 
-score = {}
+scores = {}
 
 # an instance of Chrome
 driver = webdriver.Chrome('E:/SOFTWARES/chromedriver')
@@ -10,31 +15,37 @@ driver = webdriver.Chrome('E:/SOFTWARES/chromedriver')
 #  login to HACKERRANK
 
 driver.get('https://www.hackerrank.com/dashboard')
-
 driver.maximize_window() #For maximizing window
-
+driver.implicitly_wait(5) #take a 5
 login_btn =  driver.find_element_by_xpath('//button[normalize-space()="Log In"]')
 login_btn.click()
+
+driver.implicitly_wait(5) #take a 5
 
 username_input = driver.find_element_by_id('input-1')
 password_input = driver.find_element_by_id('input-2')
 
-username_input.send_keys('someone@something.com')
-password_input.send_keys('*****')
+username_input.send_keys('########')
+password_input.send_keys('#####')
 password_input.send_keys(Keys.ENTER)
 
 # get the contest leaderboard
 
 def get_leaderboard(contestlink,leaderboard):
+    url = 'https://www.hackerrank.com/' + contestlink + leaderboard
+    driver.get(url)
+    driver.implicitly_wait(7) #wait for 7
     try:
-        error404 = driver.get_elements_by_class_name('e404-view')
-        return "404"
-    except AttributeError:
-        driver.get('https://www.hackerrank.com/' + contestlink + leaderboard)
-        driver.implicitly_wait(10) #gives an implicit wait for 10 seconds
         table_div = driver.find_element_by_id("leaders")
-        divs = table_div.find_elements_by_class_name('leaderboard-list-view')
-        for div in divs:
+    except:
+        try:
+            table_div = driver.find_element_by_id("leaders")
+        except:
+            pass
+        print("some error occured. Retrying")
+        #get_leaderboard(contestlink,leaderboard)
+    divs = table_div.find_elements_by_class_name('leaderboard-list-view')
+    for div in divs:
             parent_div      = div.find_elements_by_class_name('leaderboard-row')[0]
             divs_           = parent_div.find_elements_by_tag_name('div') #6 child divs in each row
             username_div    = divs_[1]
@@ -44,9 +55,34 @@ def get_leaderboard(contestlink,leaderboard):
             score_div       = divs_[3]
             p               = score_div.find_element_by_tag_name('p')
             score           = p.get_attribute('innerHTML').strip()           
-            print(hackerrank_id,score)
+            scores[hackerrank_id] = int(score[:score.index('.')])
 
-#   CONTEST LINK
-contestlink = "t13axxx"
-for i in '12345678':
-    get_leaderboard(contestlink,"/leaderboard/" + i)
+#   CONTEST DETAILS
+# 1.  contest name
+# 2.  contest date
+# 3.  contest total score
+# 4.  leaderboard tiles at bottom 
+contestlink = "t23fcxx"
+date = '14-Aug-19'
+total = 80
+pagetiles = 7
+test = 'Test2'
+
+for i in range(1,pagetiles+1):
+    res = get_leaderboard(contestlink,"/leaderboard/" + str(i))
+
+f = pd.read_excel(r'C:\Users\Arvind\Desktop\gcfl.xlsx')
+
+for col in f.columns:
+    if 'Unnamed' in col:
+        f.drop([col], axis=1, inplace=True)
+
+f['Total '] = total
+
+for key in scores.keys():
+    #row = f.loc[f['HackerRank Id'] == key].index
+    f.loc[f.loc[f['HackerRank Id'] == key].index,test] = date
+    f.loc[f.loc[f['HackerRank Id'] == key].index,'Score'] = scores[key]
+
+f.to_excel(r'C:\Users\Arvind\Desktop\gcfl.xlsx')
+print("done")
